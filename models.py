9e96c845cb2e4c89aeacf62f21afe324cdfcf0a4
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2019-03-03 18:28:30 -0800

    Update models.py

--- models.py
+++ models.py
@@ -20,8 +20,20 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        # Input: 1 x 224 x 224
         
+        self.conv1 = nn.Conv2d(1, 32, 5) # 32 x 220 x 220
+        self.pool1 = nn.MaxPool2d(2, 2) # 32 x 110 x 110
+        self.batch_norm1 = nn.BatchNorm2d(32)
+        
+        self.conv2 = nn.Conv2d(32, 64, 3) # 64 x 108 x 108
+        self.pool2 = nn.MaxPool2d(2, 2) # 64 x 54 x 54
+        self.batch_norm2 = nn.BatchNorm2d(64)
+        
+        self.fc1 = nn.Linear(64 * 54 * 54, 256)
+        self.fc1_dropout = nn.Dropout(p=0.25)
+        
+        self.fc2 = nn.Linear(256, 2*68)
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
@@ -31,7 +43,24 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool1(x)
+        x = self.batch_norm1(x)
         
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+        x = self.batch_norm2(x)
+        
+        x = x.view(x.size(0), -1)
+        
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc1_dropout(x)
+        
+        x = self.fc2(x)
+        x = F.log_softmax(x, dim=1)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
